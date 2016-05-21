aa47ad510dae82aad5d809feacab91f609aa9d3e
abeDaisuke <sanae.green2p@gmail.com>
2016-05-21 16:29:04 +0900

    リザルトのスコア表示

---
+++ Assets/Script/Score.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour {
+    public int score = 0;
+    public AudioSource bpm;
+    public AudioClip sound;
+    public Text scoreUI;
+
+    public static Score instance;
+    
+
+	void Awake() {
+	}
+
+    void Start () {
+        instance = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            addscore();
+        }
+    }
+    public void addscore()
+    {
+        score += 10;
+		bpm.PlayOneShot(sound);
+        scoreUI.text = "score:" + score;
+    }
+      
+}
