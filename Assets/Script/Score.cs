1b9d31c6e46b177d8ab594a5d3ce9d38ab2fef64
abeDaisuke <sanae.green2p@gmail.com>
2016-05-21 15:50:07 +0900

    スコアテキストの配置(未完成)

--- Assets/Script/Score.cs
+++ Assets/Script/Score.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using UnityEngine.UI;
 
 public class Score : MonoBehaviour {
-    public int score = 0;
+    public int score = 7;
     public AudioSource bpm;
     public AudioClip sound;
     
---
+++ Assets/Script/ScoreUI.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class ScoreUI : MonoBehaviour {
+
+    private Text mtext;
+    private Score scoreP;
+	// Use this for initialization
+	void Start () {
+        scoreP = GetComponent<Score>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        string ScoreT = scoreP.score.ToString("000");
+        mtext.text = "Score:" + ScoreT;
+	}
+}
