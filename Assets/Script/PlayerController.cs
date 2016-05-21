c46096c6895d90dced9e0737dbc5517bcb147f43
Funihiko <teiruzu3111sidos@docomo.ne.jp>
2016-05-21 16:15:15 +0900

    jiojoiy

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -4,15 +4,17 @@ using System.Collections;
 public class PlayerController : MonoBehaviour {
 
 	Score score;
+    public Rigidbody2D move;
 
 	// Use this for initialization
 	void Start () {
 		score = FindObjectOfType<Score>();
+        move = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+    
 	}
 
 	void OnTriggerEnter2D(Collider2D col) {
