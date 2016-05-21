1cf01d1a63fc6659ded9d9a7bb052e38528ddf6c
abeDaisuke <sanae.green2p@gmail.com>
2016-05-21 14:52:29 +0900

    タイマースクリプトの追加

---
+++ Assets/Script/Timer.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Timer : MonoBehaviour {
+
+    public float firstTime=5.0f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        firstTime -= Time.deltaTime;
+        if(firstTime<0)
+        {
+            Debug.Break();
+        }
+	}
+}
+
